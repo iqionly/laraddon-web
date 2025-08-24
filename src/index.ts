d4f286eca1920d8f9a81524c29ee190e6262208b
iqionly <mochammadrizkyashyari@gmail.com>
2025-08-25 00:43:10 +0700

    Initialize

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import "./styles/tailwind.css";
+
+// export { default as Button } from "./components/Button.vue";
+// export { default as Card } from "./components/Card.vue";
+// export more components here
